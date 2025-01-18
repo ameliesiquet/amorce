f61a01177f31cf3eadcb4e94d3194c669872ed24
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-01-18 17:18:35 +0100

    debug

--- resources/views/livewire/pages/dashboard/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard/dashboard.blade.php
@@ -1,10 +1,9 @@
 <main class="flex flex-col gap-10 p-4 sm:p-8  ml-72">
-    <h1 class="mb-8 text-3xl font-bold">Bonjour {{ Auth::user()->name }} 👋🏻</h1>
+    <h1 class="mb-8 text-3xl font-bold">
+        Bonjour {{ Auth::check() ? Auth::user()->name : 'Guest' }} 👋🏻
+    </h1>
     <div class="flex gap-10 items-start">
         <livewire:pages.dashboard.calendar/>
         <livewire:pages.dashboard.recent-projects/>
     </div>
 </main>
-
-
-
