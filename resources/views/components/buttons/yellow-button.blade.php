ab70ca3911eaecc223564a7761155d5c49f527d4
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-08-05 17:23:13 +0200

    add hover of buttons

--- resources/views/components/buttons/yellow-button.blade.php
+++ resources/views/components/buttons/yellow-button.blade.php
@@ -1,4 +1,4 @@
-<button class="flex gap-1.5 text-xs lg:text-sm xl:text-l items-center px-2 py-2 lg:px-3 md:py-3 my-auto text-black bg-amber-200 rounded-xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)] whitespace-nowrap"
+<button class="flex gap-1.5 text-xs lg:text-sm xl:text-l items-center px-2 py-2 lg:px-3 md:py-3 my-auto text-black bg-amber-200 rounded-xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)] whitespace-nowrap hover:bg-amber-100"
         type="submit">
     {{ $slot }}
 </button>
