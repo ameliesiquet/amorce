4b19dc67a8ab08e1489967236811b9ff0daa4f13
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-08-26 20:39:56 +0200

    update user

--- resources/views/livewire/pages/dashboard/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard/dashboard.blade.php
@@ -1,6 +1,6 @@
 <main class="flex flex-col gap-10 p-4 sm:p-8  ml-72">
     <h1 class="mb-8 text-3xl font-bold">
-        Bonjour {{ Auth::check() ? Auth::user()->name : 'Guest' }} 👋🏻
+        Bonjour👋🏻
     </h1>
     <div class="flex gap-10 items-start">
         <livewire:pages.dashboard.calendar/>
