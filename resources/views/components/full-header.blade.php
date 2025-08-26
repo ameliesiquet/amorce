8ef4f8d6ef44973a218fca440b710cf553bfbb47
Amélie <120989561+ameliesiquet@users.noreply.github.com>
2025-08-26 21:57:48 +0200

    update

--- resources/views/components/full-header.blade.php
+++ resources/views/components/full-header.blade.php
@@ -4,9 +4,11 @@
 
     {{-- Sidebar --}}
     <aside id="default-sidebar"
-           class="fixed top-0 left-0 z-40 w-64 h-screen min-h-screen transition-transform -translate-x-full lg:translate-x-0 lg:static lg:w-64"
+           class="fixed top-0 left-0 z-40 w-64 h-screen min-h-screen overflow-y-auto bg-zinc-900
+              transition-transform transform -translate-x-full lg:translate-x-0"
            aria-label="Sidebar">
-        <div class="h-full px-3 py-4 overflow-y-auto bg-zinc-900">
+
+    <div class="h-full px-3 py-4 overflow-y-auto bg-zinc-900">
 
             {{-- Dashboard --}}
             <div class="mb-4 pl-8 pr-8 pt-2 pb-2
